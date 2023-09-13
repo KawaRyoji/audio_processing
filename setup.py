b70ee01ea9c7f4100d60e33ae126bcbcd3721528
kawa <180441035@ccalumni.meijo-u.ac.jp>
2023-09-13 15:00:14 +0900

    インストールのバグ修正

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name="audio_processing",
     version="1.0.0",
     requires=open("./requirements.txt").read().splitlines(),
-    packages=setuptools.find_packages("./audio_processing"),
+    packages=setuptools.find_packages(),
 )
