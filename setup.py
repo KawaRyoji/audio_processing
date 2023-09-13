beedf3e045fcaff4ae55a5c5ab60664f415d6b68
kawa <180441035@ccalumni.meijo-u.ac.jp>
2023-09-13 15:44:10 +0900

    Revert "インストールのバグ修正"
    This reverts commit 68e7320ec643b1fd0444ec7cc51818f4c9521b27.

--- setup.py
+++ setup.py
@@ -4,6 +4,4 @@ setuptools.setup(
     name="audio_processing",
     version="1.0.0",
     requires=open("./requirements.txt").read().splitlines(),
-    packages=["audio_processing"],
-    package_dir={"audio_processing": "."}
 )
